fbdb899a6db17b8666221910ebab361042ea5004
JuckM4n <nik_morozov0@mail.ru>
2024-02-11 23:02:01 +0300

    comments have been corrected

--- js/burger.js
+++ js/burger.js
@@ -2,7 +2,7 @@ let burger = document.querySelector('.burger');
 let overlay = document.querySelector('.overlay');
 let body = document.querySelector('body');
 
-let links = document.querySelectorAll('.menu__link');[1, 2, 3, 4, 5, 6, 7]
+let links = document.querySelectorAll('.overlay-menu__link');[1, 2, 3, 4, 5, 6, 7]
 
 links.forEach(function (element) {
   element.addEventListener('click', toggleMenu);
