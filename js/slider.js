e432163380f490ac3df22463d2e9f415b2f6d544
JuckM4n <nik_morozov0@mail.ru>
2024-02-26 16:38:00 +0300

    fixing button "Заказать"

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,17 @@
+const mobileDetection = new MobileDetect(window.navigator.userAgent);
+const Mobile = mobileDetection.mobile();
+
 const slider = $(".slider__list").bxSlider({
   pager: false,
-  controls: false
+  controls: false,
+  touchEnabled: false
 });
+
+if (Mobile) {
+  slider.bxSlider({
+    touchEnabled: true
+  });
+}
 
 $(".slider__arrow--left").click(e => {
   e.preventDefault();
